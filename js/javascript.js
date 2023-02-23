12095b437277009db0b405e7bec121411296263c
TacticalTruth <123516809+TacticalTruth@users.noreply.github.com>
2023-02-22 19:09:10 -0600

    added some JS
    commented out most of the scripts, I have no idea how to write JS so it's pretty bad. Currently the only part that is turned on is the Unsorted movies toggle.

--- js/javascript.js
+++ js/javascript.js
@@ -12,3 +12,133 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// function showMovies() {
+//     var x = document.getElementById("Nolan")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies2() {
+//     var x = document.getElementById("Tom")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies3() {
+//     var x = document.getElementById("DWash")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies4() {
+//     var x = document.getElementById("SciCom")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies5() {
+//     var x = document.getElementById("SciFi")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies6() {
+//     var x = document.getElementById("Western")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies7() {
+//     var x = document.getElementById("War")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies8() {
+//     var x = document.getElementById("IV")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies9() {
+//     var x = document.getElementById("JB")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies10() {
+//     var x = document.getElementById("Spider")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies11() {
+//     var x = document.getElementById("HeartWarm")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+// function showMovies12() {
+//     var x = document.getElementById("Love&Time")
+//     if (x.classList === "RowWrap") {
+//         x.classList.add("Hide");
+//     }
+//     else {
+//         x.classList.toggle("Hide")
+//     }
+// }
+
+function showMovies13() {
+    var x = document.getElementById("Unsort")
+    if (x.classList === "RowWrap") {
+        x.classList.add("Hide");
+    }
+    else {
+        x.classList.toggle("Hide")
+    }
+}
